cfbf6ab6986e6c168dde3063c30a7046f1aeb05e
Brent Martin <bmart35@gmail.com>
2016-04-01 15:47:54 -0500

    URL's are now linking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
   "<h1>Hello!</h1>".html_safe
   end
 
+  # def show_link(converted_URL, URL)
+  #   # This is the same as Rail's #link_to
+  #   "<a href=#{converted_URL}>#{URL}</a>".html_safe
+  # end
+
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,9 @@ class Link < ActiveRecord::Base
   has_one :vote
 
 
+
+  def converted_url
+    "http://" + self.URL
+  end
+
 end

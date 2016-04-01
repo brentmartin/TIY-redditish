9bb2a50e7ffed148c92b34d425c72ace8c85f2b2
Brent Martin <bmart35@gmail.com>
2016-04-01 12:39:49 -0500

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+id = 0
+10.times do |id|
+  link_title = Faker::Company.catch_phrase
+              + " "
+              + ["has found", "is the original","meets","has gone","went","shocked by","is the new","made the","killed the"].sample
+              + " "
+              + Faker::Hipster.word
+  Link.create(title: link_title,
+              URL: Faker::Hacker.adjective+Faker::Hacker.noun+".com")
+  id = id + 1
+  Vote.create(link_id: id)
+end

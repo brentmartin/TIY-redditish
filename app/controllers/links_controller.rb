8e2ed1b609ddf27da6258fd413161d00cb93cc46
Brent Martin <bmart35@gmail.com>
2016-04-01 13:02:18 -0500

    generated links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,2 +1,7 @@
 class LinksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'links/index'
+
+  get 'links/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
